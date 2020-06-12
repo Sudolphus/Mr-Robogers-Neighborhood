2fa2eb62f5f4ea70b3baa4e2dfd5c55430d85795
Micheal Hansen <micheal.hansen@gmail.com>
2020-06-12 09:00:06 -0700

    add js to collect user input

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+//business logic
+
+//user interface logic
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    const upperLimit = $("input[name=numberInput]").val();
+  })
+})
