ccd6621464eb94ed66bdf99b85c9e32e845b182d
Micheal Hansen <micheal.hansen@gmail.com>
2020-06-12 09:08:27 -0700

    add logic to create an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,16 @@
 //business logic
+const arrayGen = function(upperLimit) {
+  const numberRange = [];
+  for (let i = 0; i <= upperLimit; i++) {
+    numberRange.push(i);
+  }
+}
 
 //user interface logic
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     const upperLimit = $("input[name=numberInput]").val();
+    const outputArray = arrayGen(upperLimit);
   })
 })
